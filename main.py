f35c272f8139a384c3368b96da0f8f00cbbac56a
FredSV17 <fredvillar64@gmail.com>
2025-09-25 20:09:56 -0400

    # Small fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bb_utils import all_classes_heatmap, get_bounding_box_areas, save_bboxes
 if __name__ == "__main__":
     dl_list = [DataLoader('train'),DataLoader('test'),DataLoader('valid')]
     get_bounding_box_areas(dl_list)
-    # check_class_representation(dl_list)
-    # check_objects_per_image(dl_list)
-    # all_classes_heatmap(dl_list)
-    # save_bboxes(dl_list)
+    check_class_representation(dl_list)
+    check_objects_per_image(dl_list)
+    all_classes_heatmap(dl_list)
+    save_bboxes(dl_list)
