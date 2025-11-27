dd67e1ae5ec98253ec5e71c802c46701a30cde47
FredSV17 <fredvillar64@gmail.com>
2025-11-27 18:19:36 -0500

    # Small changes on train, removal of yolo file

--- model_training/train.py
+++ model_training/train.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 # Training with custom augmentation parameters
-model.train(data="data/data.yaml", epochs=100, hsv_h=0.03, hsv_s=0.6, hsv_v=0.5)
+#model.train(data="data/data.yaml", epochs=100, hsv_h=0.03, hsv_s=0.6, hsv_v=0.5)
 
 # Training without any augmentations (disabled values omitted for clarity)
 model.train(
@@ -18,14 +18,24 @@ model.train(
     translate=0.0,
     scale=0.0,
     fliplr=0.0,
-    mosaic=1.0,
+    mosaic=0.5,
     erasing=0.0,
     auto_augment=None,
 )
 
+# Evaluate model performance on the validation set
+metrics = model.val()
+
+# # Perform object detection on an image
+    # results = model("data/images/val/front_1707322823.png")
+    # results[0].show()
+
+# Export the model to ONNX format
+path = model.export(format="onnx")  # return path to exported model
+
 # Training with custom Albumentations transforms (Python API only)
-custom_transforms = [
-    A.Blur(blur_limit=7, p=0.5),
-    A.CLAHE(clip_limit=4.0, p=0.5),
-]
-model.train(data="data/data.yaml", epochs=100, augmentations=custom_transforms)
+# custom_transforms = [
+#     A.Blur(blur_limit=7, p=0.5),
+#     A.CLAHE(clip_limit=4.0, p=0.5),
+# ]
+# model.train(data="data/data.yaml", epochs=100, augmentations=custom_transforms)
